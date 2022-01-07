23d5f59322cc8832ba35845ad0aa692da14a0545
qiuzhanjia <qiuzhanjia@lizhi.fm>
2022-01-07 16:25:06 +0800

    csv、docx

---
+++ docx_first.py
@@ -0,0 +1,23 @@
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='Intense Quote')
+
+document.add_paragraph(
+    'first item in unordered list', style='List Bullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='List Number'
+)
+
+document.save('demo.docx')
